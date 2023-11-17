8160095cd3bd4e50eb4d253a115f9aa50b90b9c0
shpkc <shpkc@naver.com>
2023-11-17 15:48:45 +0900

    feat : 임시로 리스트 구현

--- apps/tech_posts/src/page-modules/home/components/Home.tsx
+++ apps/tech_posts/src/page-modules/home/components/Home.tsx
@@ -1,18 +1,19 @@
 import { Spacer, Stack, Text } from "pure-strike-ui";
-import { TechPostItem } from "./TechPostItem";
+import { TechPostList } from "./TechPostList";
 
 const Home = () => {
   return (
     <Stack paddingTop={60}>
       <Spacer height={100} />
       <Text typo={"Title48Black"} align={"center"}>
-        다양한 국내 기술 블로그들을 서핑하세요
+        다양한 기술 블로그들을 살펴보세요
       </Text>
       <Spacer height={20} />
       <Text typo={"MainText20Medium"} align={"center"} color={"gray-600"}>
         국내외 흥미로운 테크 기술 글들을 소개시켜드려요
       </Text>
-      <TechPostItem />
+      <Spacer height={100} />
+      <TechPostList />
     </Stack>
   );
 };
