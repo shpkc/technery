af8d97a4c5c78de3c6e511174e0df0f3586e10c1
shpkc <shpkc@naver.com>
2023-11-17 15:47:32 +0900

    feat : 공용 Grid component 추가

---
+++ packages/pure-strike-ui/src/components/Grid/Grid.tsx
@@ -0,0 +1,9 @@
+import React, { forwardRef } from "react";
+import * as Styled from "./Grid.styled";
+import { GridProps } from "./Grid.types";
+
+export const Grid = forwardRef(
+  ({ title, children, ...rest }: GridProps, forwardRef) => {
+    return <Styled.GridWrapper {...rest}>{children}</Styled.GridWrapper>;
+  }
+);
