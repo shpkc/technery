ccfa335a78b91acb3847416261a7bca3356eb9b3
shpkc <shpkc@naver.com>
2023-11-20 21:28:37 +0900

    feat : Footer Layout에 추가

--- apps/tech_posts/src/components/layout/footer/Footer.tsx
+++ apps/tech_posts/src/components/layout/footer/Footer.tsx
@@ -1,11 +1,11 @@
 import { Stack, Text } from "pure-strike-ui";
+import { GITHUB_LINK } from "src/constants/link";
 
 export const Footer = () => {
-  const GITHUB_LINK = "https://github.com/shpkc/tech-articles";
   return (
     <Stack paddingTop={100} paddingBottom={30}>
       <Text align={"center"} typo={"MainText16Bold"}>
-        © 2023 •{" "}
+        © 2023 TECH PIN •{" "}
         <a href={GITHUB_LINK} target={"_blank"}>
           Github
         </a>
