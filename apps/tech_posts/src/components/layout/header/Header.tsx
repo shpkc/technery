cd7b8be4695b603fdfed1999babbc6e2fe175a16
shpkc <shpkc@naver.com>
2023-11-20 21:28:37 +0900

    feat : Footer Layout에 추가

--- apps/tech_posts/src/components/layout/header/Header.tsx
+++ apps/tech_posts/src/components/layout/header/Header.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link";
 import { SearchIcon } from "pure-strike-icons";
 import { Stack, Text, Icon, Flex } from "pure-strike-ui";
+import { ZIndex } from "~foundation";
 
 export const Header = () => {
   return (
-    <Stack position={"fixed"} top={0} left={0} paddingAll={16} height={60}>
+    <Stack
+      position={"fixed"}
+      top={0}
+      left={0}
+      paddingAll={16}
+      height={60}
+      zIndex={ZIndex.Header}
+      backgroundColor={"white"}
+    >
       <Flex direction={"row"} justify={"space-between"} alignment={"center"}>
         <Link href={"/"}>
-          <Text typo={"Title24Bold"}>TECH ARTICLES</Text>
+          <Text typo={"Title24Bold"}>TECH PIN</Text>
         </Link>
         <Icon source={SearchIcon} />
       </Flex>
