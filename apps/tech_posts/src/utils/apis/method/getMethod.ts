ae26001751ee621df6a1a9a6100f7c5fd718bf00
shpkc <shpkc@naver.com>
2023-11-21 21:23:40 +0900

    feat : api 구현부 추가

---
+++ apps/tech_posts/src/utils/apis/method/getMethod.ts
@@ -0,0 +1,20 @@
+import instance from "utils/apis/config/requestConfig";
+import { AxiosResponse } from "axios";
+
+// NOTE : get method 처리 구현부
+export const getMethod =
+  <TOptions>({ url, options }: { url: string; options: TOptions }) =>
+  async (mapper?: (data: AxiosResponse["data"]) => void) => {
+    const axiosConfig = instance;
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+    try {
+      const response = await axiosConfig({
+        method: "get",
+        url: `${baseUrl}${url}`,
+        ...options,
+      });
+      return mapper ? mapper(response.data) : response.data;
+    } catch (err) {
+      console.warn(err);
+    }
+  };
