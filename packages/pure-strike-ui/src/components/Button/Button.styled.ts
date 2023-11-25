9f7bf49475ba3f4255c86ea22370692b6332c7bb
shpkc <shpkc@naver.com>
2023-11-25 17:55:41 +0900

    style : Button Primary 속성 변경

--- packages/pure-strike-ui/src/components/Button/Button.styled.ts
+++ packages/pure-strike-ui/src/components/Button/Button.styled.ts
@@ -10,7 +10,7 @@ import { ButtonStyleVariants } from "~/foundation/button/buttonVariants";
 
 // NOTE: ButtonSize에 따른 버튼의 min-width, height
 const BUTTON_SIZE_VALUE = {
-  [ButtonSize.SMALL]: { width: "111px", height: "32px" },
+  [ButtonSize.SMALL]: { width: "42px", height: "34px" },
   [ButtonSize.MEDIUM]: { width: "131px", height: "44px" },
   [ButtonSize.STANDARD]: { width: "211px", height: "56px" },
   [ButtonSize.MODAL]: { width: "143px", height: "44px" },
@@ -53,7 +53,8 @@ const getTextStyleFromVariant = ({
   switch (styleVariant) {
     case ButtonStyleVariant.Primary:
       return css`
-        color: white;
+        color: ${Palette["black-origin"]};
+        font-weight: 700;
       `;
     case ButtonStyleVariant.Secondary:
       return css`
@@ -64,7 +65,6 @@ const getTextStyleFromVariant = ({
 
 // NOTE : 기본 버튼 스타일
 export const ButtonWrapper = styled.button<ButtonWrapperProps>`
-  width: 100%;
   position: relative;
   box-sizing: border-box;
   cursor: pointer;
@@ -79,6 +79,7 @@ export const ButtonWrapper = styled.button<ButtonWrapperProps>`
 
 // NOTE : 기본 내부 텍스트 스타일
 export const ContentText = styled(Text)`
+  padding: 0 4px;
   font-size: 16px;
   line-height: 24px;
   text-align: center;
