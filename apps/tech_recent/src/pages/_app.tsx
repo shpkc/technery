e1d8dff327d3007c78960bf9d30ce2382a9886c8
ParkSiHyungDshare <148029867+ParkSiHyungDshare@users.noreply.github.com>
2023-11-28 17:05:05 +0900

    feat : add GA4 script

--- apps/tech_recent/src/pages/_app.tsx
+++ apps/tech_recent/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { NextSeo } from "next-seo";
 import { DEFAULT_SEO } from "src/constants/seo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Script from "next/script";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +20,17 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});
+        `}
+      </Script>
       {seoData ? <NextSeo {...seoData} /> : <NextSeo {...DEFAULT_SEO} />}
       <Component {...pageProps} />
       <Analytics mode={"production"} />;
