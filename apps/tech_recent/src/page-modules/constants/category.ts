b52af3cfc49e7b44e497c9a361853d958a807d05
shpkc <shpkc@naver.com>
2023-11-29 23:57:58 +0900

    feat : category 종류 추가

--- apps/tech_recent/src/page-modules/constants/category.ts
+++ apps/tech_recent/src/page-modules/constants/category.ts
@@ -1,7 +1,13 @@
 export const CATEGORY_LIST = [
   { name: "All", value: "" },
-  { name: "FrontEnd", value: "Front" },
-  { name: "BackEnd", value: "BackEnd" },
+  { name: "Frontend", value: "Frontend" },
+  { name: "Backend", value: "Frontend" },
   { name: "Mobile", value: "Mobile" },
   { name: "DevOps", value: "DevOps" },
+  { name: "AI", value: "AI" },
+  { name: "ML", value: "ML" },
+  { name: "Data", value: "Data" },
+  { name: "QA", value: "QA" },
+  { name: "Event", value: "Event" },
+  { name: "ETC", value: "ETC" },
 ];
