7084e916646e4dccc5ed48aa4ed025cd488829af
shpkc <shpkc@naver.com>
2023-12-06 21:43:00 +0900

    feat : 반응형 필요한곳 추가 및 속성 변경

--- apps/technery/src/components/layout/footer/Footer.tsx
+++ apps/technery/src/components/layout/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { GITHUB_LINK } from "src/constants/link";
 
 export const Footer = () => {
   return (
-    <Stack paddingTop={100} paddingBottom={30}>
+    <Stack padding={["60px 0 30px 0", "100px 0 30px 0"]}>
       <Text align={"center"} typo={"MainText16Bold"}>
         © 2023 Teveloper •{" "}
         <a href={GITHUB_LINK} target={"_blank"}>
--- apps/technery/src/components/layout/header/Header.tsx
+++ apps/technery/src/components/layout/header/Header.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
-import { SearchIcon } from "pure-strike-icons";
 import { Stack, Text, Icon, Flex } from "pure-strike-ui";
 import { ZIndex } from "~foundation";
+import { FaGithub } from "react-icons/fa";
+import { GITHUB_LINK } from "src/constants/link";
 
 export const Header = () => {
   return (
@@ -9,16 +10,19 @@ export const Header = () => {
       position={"fixed"}
       top={0}
       left={0}
-      paddingAll={16}
+      padding={"16px"}
       height={60}
       zIndex={ZIndex.Header}
       backgroundColor={"white"}
+      width={"100%"}
     >
       <Flex direction={"row"} justify={"space-between"} alignment={"center"}>
         <Link href={"/"}>
           <Text typo={"Title24Bold"}>TECHNERY</Text>
         </Link>
-        <Icon source={SearchIcon} />
+        <a href={GITHUB_LINK} target={"_blank"}>
+          <FaGithub size={32} />
+        </a>
       </Flex>
     </Stack>
   );
