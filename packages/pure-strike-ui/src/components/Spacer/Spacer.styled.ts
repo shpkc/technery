23048d8ee1956ae490e5ffcc806fe8fbc31125b7
shpkc <shpkc@naver.com>
2023-12-06 21:42:16 +0900

    feat : Spacer responsive prop 일부분 추가

--- packages/pure-strike-ui/src/components/Spacer/Spacer.styled.ts
+++ packages/pure-strike-ui/src/components/Spacer/Spacer.styled.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import { getResponsiveStyles } from "~/foundation/responsive";
 import { SpacerProps } from "./Spacer.types";
 
 const Spacer = styled.div<SpacerProps>`
   width: ${({ width }) => (width ? width + "px" : "100%")};
-  height: ${({ height }) => (height ? height + "px" : "100%")};
+  ${(props) => getResponsiveStyles("height", props.height)};
 `;
 
 export default Spacer;
