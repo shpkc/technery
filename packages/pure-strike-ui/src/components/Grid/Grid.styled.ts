de21bf445d8bf8edc911841e9aa7fef422ac8b06
shpkc <shpkc@naver.com>
2023-12-06 21:42:28 +0900

    feat : Grid responsive prop 일부분 추가

--- packages/pure-strike-ui/src/components/Grid/Grid.styled.ts
+++ packages/pure-strike-ui/src/components/Grid/Grid.styled.ts
@@ -1,10 +1,12 @@
 import styled, { css } from "styled-components";
+import { getResponsiveStyles } from "~/foundation/responsive";
 import { GridProps } from "./Grid.types";
 
 export const GridWrapper = styled.div<GridProps>`
   display: grid;
-  grid-template-columns: ${({ gridTemplateColums }) =>
-    `repeat(${gridTemplateColums}, 1fr)`};
+  ${(props) =>
+    getResponsiveStyles("grid-template-columns", props.gridTemplateColums)};
+
   grid-column-gap: ${({ gridColumnGap }) => gridColumnGap + "px"};
   grid-row-gap: ${({ gridRowGap }) => gridRowGap + "px"};
 `;
