0afeceaa99cf16cb9339eb0b3638ff35216a3acd
shpkc <shpkc@naver.com>
2023-12-26 21:34:38 +0900

    style : Icon 필요없는 속성 삭제

--- packages/pure-strike-ui/src/components/Icon/Icon.styled.ts
+++ packages/pure-strike-ui/src/components/Icon/Icon.styled.ts
@@ -3,15 +3,6 @@ import { MarginProps } from "~/types/componentProps";
 import { IconStyledProps } from "./Icon.types";
 import { ColorPaletteProps, Palette } from "~/foundation";
 
-const getMargin = ({
-  marginTop,
-  marginRight,
-  marginBottom,
-  marginLeft,
-}: MarginProps): string => {
-  return `${marginTop}px ${marginRight}px ${marginBottom}px ${marginLeft}px`;
-};
-
 const getPaletteColor = (color: ColorPaletteProps) => {
   return Palette[color];
 };
@@ -19,7 +10,6 @@ const getPaletteColor = (color: ColorPaletteProps) => {
 const Icon = styled.svg<IconStyledProps>`
   cursor: pointer;
   flex: 0 0 auto;
-  margin: ${getMargin};
   color: ${(props) => getPaletteColor(props.color as ColorPaletteProps)};
 `;
 
--- packages/pure-strike-ui/src/components/Icon/Icon.tsx
+++ packages/pure-strike-ui/src/components/Icon/Icon.tsx
@@ -6,20 +6,7 @@ import { noop } from "~/utils/funtionUtils";
 
 export const Icon = forwardRef(
   (
-    {
-      source,
-      size = IconSize.Size32,
-      color,
-      marginTop = 0,
-      marginRight = 0,
-      marginBottom = 0,
-      marginLeft = 0,
-      marginX = 0,
-      marginY = 0,
-      marginAll = 0,
-      onClick = noop,
-      style,
-    }: IconProps,
+    { source, size = IconSize.Size32, color, onClick = noop, style }: IconProps,
     forwardRef
   ) => {
     const handleClick = React.useCallback<MouseEventHandler>(
@@ -34,10 +21,6 @@ export const Icon = forwardRef(
         width={size}
         height={size}
         color={color}
-        marginTop={marginTop || marginY || marginAll}
-        marginRight={marginRight || marginX || marginAll}
-        marginBottom={marginBottom || marginY || marginAll}
-        marginLeft={marginLeft || marginX || marginAll}
         onClick={handleClick}
         style={style}
       />
