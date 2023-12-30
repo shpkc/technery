cab9ad856a128ff7d1f2a835ea3d46379dcce026
shpkc <shpkc@naver.com>
2023-12-30 18:06:07 +0900

    feat : add Footer to Layout

--- apps/technery/src/components/layout/footer/Footer.tsx
+++ apps/technery/src/components/layout/footer/Footer.tsx
@@ -1,14 +1,10 @@
 import { Stack, Text } from "pure-strike-ui";
-import { GITHUB_LINK } from "src/constants/link";
 
 export const Footer = () => {
   return (
     <Stack padding={["60px 0 30px 0", "100px 0 30px 0"]}>
-      <Text align={"center"} typo={"MainText16Bold"}>
-        © 2023 Teveloper •{" "}
-        <a href={GITHUB_LINK} target={"_blank"}>
-          Github
-        </a>
+      <Text align={"center"} typo={"Text16Bold"}>
+        © 2023 Technery
       </Text>
     </Stack>
   );
