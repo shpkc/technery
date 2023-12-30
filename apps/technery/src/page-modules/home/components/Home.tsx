e7ecfefd5ebfa71f10987cba9bfa9ada0b2e1940
shpkc <shpkc@naver.com>
2023-12-30 18:07:13 +0900

    feat : Home 페이지 구현 완성

--- apps/technery/src/page-modules/home/components/Home.tsx
+++ apps/technery/src/page-modules/home/components/Home.tsx
@@ -1,15 +1,18 @@
 import { Spacer, Stack, Text } from "pure-strike-ui";
 import { RecentServices } from "./RecentServices";
-import { TrendingServices } from "./TrendingServices";
+import { TrendServices } from "./TrendServices";
 import { TrendingTopics } from "./TrendingTopics";
 
 const Home = () => {
   return (
     <Stack padding={"60px 0 0 0"} width={["100%", "1100px"]} margin={"0 auto"}>
-      <Spacer height={32} />
+      <Spacer height={["16px", "32px"]} />
       <TrendingTopics />
-      <Spacer height={100} />
-      <TrendingServices />
+      <Spacer height={["32px", "80px"]} />
+      <TrendServices />
+      <Spacer height={["32px", "80px"]} />
+      <RecentServices />
+      <Spacer height={["32px", "80px"]} />
     </Stack>
   );
 };
