adc548b72081e0581bc5a442d2fe57d2f395a40d
shpkc <shpkc@naver.com>
2024-01-03 22:26:19 +0900

    feat : service detail page 모바일 대응

--- apps/technery/src/page-modules/servicies/detail/components/ServiceDetail.tsx
+++ apps/technery/src/page-modules/servicies/detail/components/ServiceDetail.tsx
@@ -42,25 +42,29 @@ const ServiceDetail = () => {
   return (
     <Stack padding={"60px 0 0 0"}>
       <Stack width={["100%", "650px"]} margin={"0 auto"}>
-        <Spacer height={["80px", "80px"]} />
+        <Spacer height={["40px", "80px"]} />
         <Text typo={"Text32Bold"} align={"center"}>
           {serviceData.name}
         </Text>
-        <Spacer height={["80px", "24px"]} />
-        <Text typo={"Text24Medium"} align={"center"}>
-          {serviceData.summary}
-        </Text>
-        <Spacer height={["80px", "60px"]} />
+        <Spacer height={["20px", "24px"]} />
+        <Stack padding={["16px", "0px"]}>
+          <Text typo={"Text24Medium"} align={"center"}>
+            {serviceData.summary}
+          </Text>
+        </Stack>
+        <Spacer height={["40px", "60px"]} />
         <Flex gap={20} overflow={"scroll"}>
           {serviceData.images.map((item) => (
             <Image src={item.url} key={item.url} height={"285px"} />
           ))}
         </Flex>
-        <Spacer height={["80px", "60px"]} />
-        <Text typo={"Text18Regular"} whitespace={"pre-wrap"}>
-          {serviceData.description}
-        </Text>
-        <Spacer height={["80px", "60px"]} />
+        <Spacer height={["40px", "60px"]} />
+        <Stack padding={["16px", "0px"]}>
+          <Text typo={"Text18Regular"} whitespace={"pre-wrap"}>
+            {serviceData.description}
+          </Text>
+        </Stack>
+        <Spacer height={["40px", "60px"]} />
         <Flex justify={"center"}>
           <Link href={serviceData.link} target={"_blank"}>
             <Stack width={"160px"}>
@@ -88,13 +92,14 @@ const ServiceDetail = () => {
       <Flex justify={"center"} alignment={"center"}>
         <BsTagFill size={28} />
         <Spacer width={["12px", "4px"]} />
-        <Stack width={"60px"}>
+        <Stack minWidth={"60px"}>
           <Link href={`/topics/${serviceData.topics.id}`}>
             <Button
               styleVariant={ButtonStyleVariant.Outline}
               size={ButtonSize.SMALL}
               text={serviceData.topics.title}
               borderRadius={"20px"}
+              style={{ padding: "4px 8px" }}
             />
           </Link>
         </Stack>
