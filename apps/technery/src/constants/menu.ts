42e71611214743c997847962f3642bb5423a6d88
shpkc <shpkc@naver.com>
2024-01-04 21:30:34 +0900

    feat : link & menu 값들 분리

---
+++ apps/technery/src/constants/menu.ts
@@ -0,0 +1,11 @@
+export const MENU_LIST = [
+  { title: "Topics", link: "/topics" },
+  { title: "Tech Blogs", link: "/techBlogs" },
+  {
+    title: "Submit",
+    link: "/submit",
+    onClick: () => {
+      window.open("");
+    },
+  },
+];
